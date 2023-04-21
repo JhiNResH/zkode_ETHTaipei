96efa9f83496e6bc2df354d2615636da7e8b3ecb
chen-yanlong <alan900801@gmail.com>
2023-04-21 20:13:09 +0800

    add scripts in contracts

--- src/contracts/scripts/deploy.ts
+++ src/contracts/scripts/deploy.ts
@@ -1,24 +1,48 @@
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat'
+import * as fs from 'fs'
+import * as path from 'path'
+import { deployUnirep } from '@unirep/contracts/deploy/index.js'
+import * as hardhat from 'hardhat'
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+const epochLength = 300
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+deployApp().catch((err) => {
+    console.log(`Uncaught error: ${err}`)
+    process.exit(1)
+})
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+export async function deployApp() {
+    const [signer] = await ethers.getSigners()
+    const unirep = await deployUnirep(signer)
 
-  await lock.deployed();
+    const verifierF = await ethers.getContractFactory('DataProofVerifier')
+    const verifier = await verifierF.deploy()
+    await verifier.deployed()
+    const App = await ethers.getContractFactory('UnirepApp')
+    const app = await App.deploy(unirep.address, verifier.address, epochLength)
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+    await app.deployed()
+
+    console.log(
+        `Unirep app with epoch length ${epochLength} is deployed to ${app.address}`
+    )
+
+    const config = `export default {
+    UNIREP_ADDRESS: '${unirep.address}',
+    APP_ADDRESS: '${app.address}',
+    ETH_PROVIDER_URL: '${hardhat.network.config.url ?? ''}',
+    ${
+        Array.isArray(hardhat.network.config.accounts)
+            ? `PRIVATE_KEY: '${hardhat.network.config.accounts[0]}',`
+            : `/**
+      This contract was deployed using a mnemonic. The PRIVATE_KEY variable needs to be set manually
+    **/`
+    }
+  }
+  `
+
+    const configPath = path.join(__dirname, '../../../config.ts')
+    await fs.promises.writeFile(configPath, config)
+
+    console.log(`Config written to ${configPath}`)
 }
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
