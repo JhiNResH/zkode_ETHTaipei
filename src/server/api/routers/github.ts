daeee296c25bcadb958e82ea68dfe5d5a6cbe4d5
Keenan Thompson <keenan.thompson67@gmail.com>
2023-04-23 06:54:33 +0800

    frontend: error handling

--- src/server/api/routers/github.ts
+++ src/server/api/routers/github.ts
@@ -1,13 +1,14 @@
-import { z } from "zod";
-import { protectedProcedure } from "../trpc";
-import { createTRPCRouter } from "../trpc";
-import axios from "axios";
-import { GetRepositoriesResponse, GithubReposResponse } from "./types";
-import fs from "fs";
-import { promisify } from "util";
-import AdmZip from "adm-zip";
-import { resolveCoderRepos } from "./resolveCoderRepos";
-import crypto from "crypto";
+import { z } from 'zod';
+import { protectedProcedure } from '../trpc';
+import { createTRPCRouter } from '../trpc';
+import axios from 'axios';
+import { GetRepositoriesResponse, GithubReposResponse } from './types';
+import fs from 'fs';
+import { promisify } from 'util';
+import AdmZip from 'adm-zip';
+import { resolveCoderRepos } from './resolveCoderRepos';
+import crypto from 'crypto';
+import { TRPCError } from '@trpc/server';
 
 const writeFileAsync = promisify(fs.writeFile);
 const mkdirSync = promisify(fs.mkdirSync);
@@ -20,24 +21,31 @@ export const githubRouter = createTRPCRouter({
       })
     )
     .query(async ({ input }): Promise<GetRepositoriesResponse> => {
-      const { accessToken } = input;
+      try {
+        const { accessToken } = input;
 
-      if (!accessToken) return { repositories: [] };
+        if (!accessToken) return { repositories: [] };
 
-      const response = await axios.get<GithubReposResponse[]>(
-        "https://api.github.com/user/repos",
-        {
+        const response = await axios.get<GithubReposResponse[]>('https://api.github.com/user/repos', {
           headers: { Authorization: `token ${accessToken}` },
-        }
-      );
+        });
 
-      const { data } = response;
+        const { data } = response;
 
-      const privateRepos = data.filter((repo) => repo.private);
+        const privateRepos = data.filter((repo) => repo.private);
 
-      return {
-        repositories: privateRepos,
-      };
+        return {
+          repositories: privateRepos,
+        };
+      } catch (error) {
+        const errorAsError = error as Error;
+
+        throw new TRPCError({
+          code: 'INTERNAL_SERVER_ERROR',
+          message: errorAsError.message,
+          cause: errorAsError,
+        });
+      }
     }),
   fetchAndSaveRepositories: protectedProcedure
     .input(
@@ -47,61 +55,63 @@ export const githubRouter = createTRPCRouter({
       })
     )
     .mutation(async ({ ctx, input }) => {
-      const { accessToken, repositoryIds } = input;
+      try {
+        const { accessToken, repositoryIds } = input;
 
-      const repositories = await Promise.all(
-        repositoryIds.map(async (repositoryId) => {
-          const response = await axios.get(
-            `https://api.github.com/repositories/${repositoryId}`,
-            {
+        const repositories = await Promise.all(
+          repositoryIds.map(async (repositoryId) => {
+            const response = await axios.get(`https://api.github.com/repositories/${repositoryId}`, {
               headers: { Authorization: `token ${accessToken}` },
-            }
-          );
+            });
 
-          const repo = response.data;
+            const repo = response.data;
 
-          const archiveUrl = repo.archive_url
-            .replace("{archive_format}", "zipball")
-            .replace("{/ref}", "/master");
+            const archiveUrl = repo.archive_url.replace('{archive_format}', 'zipball').replace('{/ref}', '/master');
 
-          const archiveResponse = await axios.get(archiveUrl, {
-            responseType: "arraybuffer",
-            headers: { Authorization: `token ${accessToken}` },
-          });
+            const archiveResponse = await axios.get(archiveUrl, {
+              responseType: 'arraybuffer',
+              headers: { Authorization: `token ${accessToken}` },
+            });
 
-          const key = crypto
-            .createHash("sha256")
-            .update(repo.id.toString())
-            .digest("hex");
+            const key = crypto.createHash('sha256').update(repo.id.toString()).digest('hex');
 
-          // Create the directory if it does not exist
-          mkdirSync("./savedRepos/zip", { recursive: true });
+            // Create the directory if it does not exist
+            mkdirSync('./savedRepos/zip', { recursive: true });
 
-          // Save the fetched code to disk
-          const filePath = `./savedRepos/zip/${key}.zip`;
+            // Save the fetched code to disk
+            const filePath = `./savedRepos/zip/${key}.zip`;
 
-          await writeFileAsync(filePath, archiveResponse.data);
+            await writeFileAsync(filePath, archiveResponse.data);
 
-          const zip = new AdmZip(archiveResponse.data);
+            const zip = new AdmZip(archiveResponse.data);
 
-          // Create the directory if it does not exist
-          mkdirSync("./savedRepos/full", { recursive: true });
+            // Create the directory if it does not exist
+            mkdirSync('./savedRepos/full', { recursive: true });
 
-          // Unzip the fetched repository and save it as a folder
-          zip.extractAllTo(`./savedRepos/full/${key}`, true);
+            // Unzip the fetched repository and save it as a folder
+            zip.extractAllTo(`./savedRepos/full/${key}`, true);
 
-          return key;
-        })
-      );
+            return key;
+          })
+        );
 
-      await resolveCoderRepos({
-        ctx,
-        keys: repositories,
-      });
+        await resolveCoderRepos({
+          ctx,
+          keys: repositories,
+        });
 
-      return {
-        success: true,
-      };
+        return {
+          success: true,
+        };
+      } catch (error) {
+        const errorAsError = error as Error;
+
+        throw new TRPCError({
+          code: 'INTERNAL_SERVER_ERROR',
+          message: errorAsError.message,
+          cause: errorAsError,
+        });
+      }
     }),
 });
 
